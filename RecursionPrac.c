a82ce2f139c8d57909b549448a0348a14f916668
callumapps <callum.apps@icloud.com>
2023-12-10 18:42:25 +0000

    new changes

--- RecursionPrac.c
+++ RecursionPrac.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 
-// sript to find the factorial of numbers
-
 int Factorial (int n);
 
 int main () {
-
     int number;
-
-    printf ("Enter a number");
+    printf ("add a number");
     scanf ("%d", &number);
 
-    printf("Factotial is = %d", Factorial(number));
-    return 0;
-
+    printf("\nFactorial is : %d", Factorial(number));
 }
 
 int Factorial (int n) {
-    if (n > 0){
-        return n * Factorial(n - 1);
+    if (n > 0) {
+        return n * Factorial(n-1);
     }
     else {
         return 1;
     }
+
 }
