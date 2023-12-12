14248718b7f20260a6a3013897377c0180af077a
callumeppz <callumapps991@gmail.com>
2023-12-12 01:36:42 +0000

    Practise Done for the day, theory tomorrow

---
+++ Practise.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int multiplyNums(int num1, int num2);
+
+int main() {
+    char oper;
+    int result;
+
+    char str[20];
+
+    printf ("\nEnter your name: ");
+    fgets (str, sizeof(str), stdin); 
+
+    str[2] = 'X';
+
+    printf ("%s", str);
+
+    printf("enter an operator you would like to use for calculation: '+' '-' '*' '/'\n");
+    scanf("%c", &oper);
+
+    int num, num0;
+
+    scanf ("%d %d", &num, &num0);
+
+    switch(oper){
+        case '+':
+        result = num + num0;
+        printf("%d",result);
+        break;
+        case '-':
+        result = num - num0;
+        printf("%d", result);
+        break;
+    }
+
+    (num > num0) ? printf("\ntrue") : printf("\nfalse");
+
+    multiplyNums(3, 5);
+}
+
+
+int multiplyNums(int num1, int num2) {
+    int result = 0; 
+    for (int i = 0; i < 5; ++i) {
+        result = result + num1;
+        printf ("\n%d", result);
+    }
+    return num1;
+}
