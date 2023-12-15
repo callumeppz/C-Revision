2bc8ca239b657446d7f057b88c2a7d00e13b355d
callumeppz <callumapps991@gmail.com>
2023-12-15 00:01:49 +0000

    added comments

--- Practise/BinarySearch.c
+++ Practise/BinarySearch.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    int target = 7;
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8}; // initialises array
+    int target = 7; // initialises target
 
-    for (int i = 0; i < sizeof(arr); ++i) {
-        if (arr[i] == target) {
+    for (int i = 0; i < sizeof(arr); ++i) { // searches array
+        if (arr[i] == target) { // if i matches target
             printf ("%d", arr[i]);
-            return i;
+            return i; // ends when found
         }
     }
-    return -1;
+    return -1; // returns -1 if not found
 }
